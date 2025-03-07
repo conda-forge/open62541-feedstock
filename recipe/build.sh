fca60e5071fed36d2a50b3710f6e0cf95a711035
Silvio Traversaro <silvio@traversaro.it>
2025-03-07 19:57:13 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 mkdir build && cd build
 
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
+cmake -GNinja ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
